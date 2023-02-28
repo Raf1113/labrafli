80de2d83c6f055b487190377ef8f1aab546ce448
Raf1113 <92740192+Raf1113@users.noreply.github.com>
2023-02-28 22:09:01 +0800

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,44 @@
+library(datasets)
 library(shiny)
 
 ui <- fluidPage(
-  h2(textOutput("currentTime"))
+  titlePanel("Shiny Text"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "dataset",
+                  label = "Choose a dataset:",
+                  choices = c("rock", "pressure", "cars", "iris")),
+      numericInput(inputId = "obs",
+                   label = "Number of observations to view:",
+                   value = 10)
+    ),
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Summary", verbatimTextOutput("summary")),
+                  tabPanel("Table", tableOutput("table")
+                  )
+    )
+  )
+  )
 )
-server <- function(input, output, session) {
-  output$currentTime <- renderText({
-    invalidateLater(1000, session)
-    paste("The current time is", Sys.time())
+
+server <- function(input, output) {
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = rock,
+           "pressure" = pressure,
+           "cars" = cars,
+           "iris" = iris)
   })
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  output$table <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+ 
+}
 
 shinyApp(ui = ui, server = server)
 shiny::runApp()
