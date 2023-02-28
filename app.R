2fc87694801401087f9a3b8c44b0da7208d31527
Raf1113 <92740192+Raf1113@users.noreply.github.com>
2023-02-28 22:02:25 +0800

    Add files via upload

---
+++ app.R
@@ -0,0 +1,44 @@
+library(datasets)
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Shiny Text"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "dataset",
+                  label = "Choose a dataset:",
+                  choices = c("rock", "pressure", "cars", "iris")),
+      numericInput(inputId = "obs",
+                   label = "Number of observations to view:",
+                   value = 10)
+    ),
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Summary", verbatimTextOutput("summary")),
+                  tabPanel("Table", tableOutput("table")
+                  )
+    )
+  )
+  )
+)
+
+server <- function(input, output) {
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = rock,
+           "pressure" = pressure,
+           "cars" = cars,
+           "iris" = iris)
+  })
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  output$table <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+ 
+}
+
+shinyApp(ui = ui, server = server)
+shiny::runApp()
